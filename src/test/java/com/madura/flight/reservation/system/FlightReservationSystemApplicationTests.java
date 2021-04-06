0809b5c039128a68226c4a9fa3940967ea6a6879
madurachandima <madurachandima6@gmail.com>
2021-04-06 20:14:18 +0530

    Initial commit

---
+++ src/test/java/com/madura/flight/reservation/system/FlightReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madura.flight.reservation.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
